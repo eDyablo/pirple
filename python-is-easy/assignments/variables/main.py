688e7ed2718f089c98dd8c7eef796d9011ee0482
edyablo <eyablonsky@gmail.com>
2020-09-07 18:24:17 +0300

    first assignment

---
+++ python-is-easy/assignments/variables/main.py
@@ -0,0 +1,24 @@
+
+"""
+Homework assignment for the 'Python is easy' course by Pirple.
+
+It prints out the set of the attributes for one favorite song.
+"""
+
+Album = "Stones and Honey" # The name of the album
+Artist = "The Hardkiss" # The name of the performer
+BitsPerMinute = 83 # The tempo of the song
+Duration = 3 * 60 + 11 # The length of the song in seconds
+Genre = "Pop"
+Loudness = 0.7 # The higher the value, the louder the song
+Title = "Strange Moves (feat. Kazaky)" # The name of the song
+Year = "2014" # The release year of the recording
+
+print(Title)
+print(Artist)
+print(Duration)
+print(Album)
+print(Year)
+print(Genre)
+print(BitsPerMinute)
+print(Loudness)
