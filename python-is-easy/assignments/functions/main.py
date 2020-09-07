93cfa83fb3df2c8d4faa02663160b9f2948b4a69
edyablo <eyablonsky@gmail.com>
2020-09-07 22:06:20 +0300

    functions assignment

---
+++ python-is-easy/assignments/functions/main.py
@@ -0,0 +1,14 @@
+def artist():
+    return "The Hardkiss"
+
+def genre():
+    return "Pop"
+
+def year():
+    return 2014
+
+def restrictedContent():
+    return False
+
+def forGeneralAudiences():
+    return True
