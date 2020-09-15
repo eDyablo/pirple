879409632baf8c703b4004e305f1e4cece734fa2
edyablo <eyablonsky@gmail.com>
2020-09-15 16:25:36 +0300

    add python snowman assignment

---
+++ python-is-easy/assignments/snowman/main.py
@@ -0,0 +1,103 @@
+from os import (
+    name as os_name,
+    system as system_call,
+)
+
+from os.path import (
+    abspath,
+    dirname,
+    join as join_path,
+)
+
+class Screen:
+    def clear(self):
+        if os_name == 'nt':
+            system_call('cls')
+        else:
+            system_call('clear')
+
+    def draw(self, frame):
+        for line in frame:
+            print(line)
+
+class Input:
+    def ask(self, message):
+        answer = ''
+        while answer == '':
+            answer = input(message)
+        return answer
+
+class Art:
+    def __init__(self):
+        self.frames = []
+        self.current_frame = 0
+
+    def load(self, name):
+        frames = []
+        art_path = join_path(dirname(abspath(__file__)), join_path('arts', name))
+        with open(art_path, 'tr') as art_file:
+            frame_height = int(art_file.readline())
+            frame = []
+            line_count = 0
+            for line in art_file:
+                frame.append(line)
+                line_count += 1
+                if line_count % frame_height == 0:
+                    frames.append(frame)
+                    frame = []
+        self.frames = frames
+        self.current_frame = 0
+
+    def draw(self, screen):
+        screen.draw(self.frames[self.current_frame])
+
+    def next_frame(self):
+        self.current_frame = (self.current_frame + 1) % len(self.frames)
+
+class Riddle:
+    def __init__(self, key):
+        self.key = key
+        self.clue = ''
+
+    def guess(self, g):
+        pass
+
+    def solved(self):
+        return self.clue == self.key
+
+    def unsolved(self):
+        return self.solved() == False
+
+class Game:
+    def __init__(self):
+        self.screen = Screen()
+        self.input = Input()
+        self.art = Art()
+        self.riddle = Riddle('riddle')
+
+    def play(self):
+        self.art.load('snowman')
+        self.propose_riddle()
+        while self.in_progress():
+            self.play_round()
+        self.end()
+
+    def propose_riddle(self):
+        self.riddle = Riddle(self.input.ask('Player 1 pick a word: '))
+
+    def in_progress(self):
+        return self.riddle.unsolved()
+
+    def play_round(self):
+        self.screen.clear()
+        self.art.draw(self.screen)
+        clue = input('Player 2 guess a letter: ')
+        if clue != '':
+            self.riddle.guess(clue)
+            self.art.next_frame()
+
+    def end(self):
+        pass
+
+game = Game()
+game.play()
